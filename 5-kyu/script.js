9cb487adf595952698dbcc90ef8b42928838a883
Paul <paulemileatn@gmail.com>
2024-02-24 00:20:30 +0100

    The Hashtag Generator ⚔️

--- 5-kyu/script.js
+++ 5-kyu/script.js
@@ -9,3 +9,22 @@ const moveZeros = function (arr) {
 
   return nonZeroArray;
 };
+
+// The Hashtag Generator
+const generateHashtag = function (str) {
+  const trimmed = str.trim();
+  if (trimmed === '') return false;
+  let hashtag;
+  if (trimmed.includes(' ')) {
+    hashtag =
+      '#' +
+      trimmed
+        .split(' ')
+        .filter(word => word !== '')
+        .map(word => word.replace(word[0], word[0].toUpperCase()))
+        .join('');
+  } else {
+    hashtag = '#' + trimmed.replace(trimmed[0], trimmed[0].toUpperCase());
+  }
+  return hashtag.length > 140 ? false : hashtag;
+};
