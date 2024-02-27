3fc518b549dd33559b4fee704b3848353373c55c
Paul <paulemileatn@gmail.com>
2024-02-27 16:48:12 +0100

    Possibilities Array

--- 7-kyu/script.js
+++ 7-kyu/script.js
@@ -12,3 +12,12 @@ const getCount = function (str) {
 const filter_list = function (arr) {
   return arr.filter(el => typeof el !== 'string');
 };
+
+// Possibilities Array
+const isAllPossibilities = function (arr) {
+  const isValid = [];
+  for (let i = 0; i < arr.length; i++) {
+    isValid.push(arr.includes(i) ? true : false);
+  }
+  return !isValid.includes(false);
+};
