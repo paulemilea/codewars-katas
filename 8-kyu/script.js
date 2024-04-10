ee2ec02a7e145b784915973a3993fbb1634dd495
Paul <paulemileatn@gmail.com>
2024-04-10 10:03:34 +0200

    Rock Paper Scissors! ⚔️

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -288,3 +288,14 @@ const remainder = function (n, m) {
   const min = Math.min(n, m);
   return max % min;
 };
+
+// Rock Paper Scissors!
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!';
+  if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+  if (p2 === 'scissors' && p1 === 'paper') return 'Player 2 won!';
+  if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+  if (p2 === 'paper' && p1 === 'rock') return 'Player 2 won!';
+  if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+  if (p2 === 'rock' && p1 === 'scissors') return 'Player 2 won!';
+};
