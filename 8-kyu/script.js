86c9b3d1c22d124920651b4d0aaad05bee400836
Paul <paulemileatn@gmail.com>
2024-05-08 08:43:38 +0200

    Get Planet Name By ID 🌍

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -441,9 +441,40 @@ const toCsvText = function (array) {
 };
 
 // Contamination #1 -String-
-const contamination = function(text, char) {
+const contamination = function (text, char) {
   return text
     .split('')
-    .map((v) => v.replace(v, char))
+    .map(v => v.replace(v, char))
     .join('');
+};
+
+// Get Planet Name By ID
+const getPlanetName = function (id) {
+  let name;
+  switch (id) {
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+      break;
+  }
+  return name;
 };
