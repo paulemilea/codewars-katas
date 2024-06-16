9cfdc6a92abf0f4f5c1061b6264ad926e21993e2
Paul <paulemileatn@gmail.com>
2024-06-16 11:34:12 +0200

    Who likes it?

--- 6-kyu/script.js
+++ 6-kyu/script.js
@@ -111,3 +111,21 @@ const alphabetPosition = function (text) {
     .map(letter => alphabet.indexOf(letter.toLowerCase()) + 1)
     .join(' ');
 };
+
+// Who likes it?
+const likes = function (names) {
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+  }
+};
