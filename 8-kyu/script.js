61fabd3dc6623c5f444f4e93880316b3b37a199c
Paul <paulemileatn@gmail.com>
2024-07-29 00:15:40 +0200

    Return the day

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -824,3 +824,35 @@ const doubleInteger = i => i * 2;
 const capitalizeWord = function (word) {
   return word[0].toUpperCase() + word.slice(1);
 };
+
+// Return the day
+const whatday = function (num) {
+  let result = '';
+  switch (num) {
+    case 1:
+      result = 'Sunday';
+      break;
+    case 2:
+      result = 'Monday';
+      break;
+    case 3:
+      result = 'Tuesday';
+      break;
+    case 4:
+      result = 'Wednesday';
+      break;
+    case 5:
+      result = 'Thursday';
+      break;
+    case 6:
+      result = 'Friday';
+      break;
+    case 7:
+      result = 'Saturday';
+      break;
+    default:
+      result = 'Wrong, please enter a number between 1 and 7';
+      break;
+  }
+  return result;
+};
