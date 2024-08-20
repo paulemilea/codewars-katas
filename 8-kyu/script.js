74efba37f63b5a46b0ca1071736771e23ca9b20e
Paul <paulemileatn@gmail.com>
2024-08-20 22:36:02 +0200

    Grader

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -939,3 +939,12 @@ Array.prototype.filter = function (func) {
   }
   return result;
 };
+
+// Grader
+const grader = function (score) {
+  if (score > 1 || score < 0.6) return 'F';
+  if (score >= 0.9) return 'A';
+  if (score >= 0.8) return 'B';
+  if (score >= 0.7) return 'C';
+  if (score >= 0.6) return 'D';
+};
