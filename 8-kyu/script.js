f614039665873aef1ca09100b7712bf3e38114a2
Paul <paulemileatn@gmail.com>
2024-10-26 11:32:57 +0200

    Switch it Up!

--- 8-kyu/script.js
+++ 8-kyu/script.js
@@ -1109,3 +1109,21 @@ const max = function (list) {
 const correct = function (string) {
   return string.replaceAll('5', 'S').replaceAll('0', 'O').replaceAll('1', 'I');
 };
+
+// Switch it Up!
+const switchItUp = function (number) {
+  const numberWords = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+  };
+
+  return numberWords[number];
+};
